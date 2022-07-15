d627388a2e5512730296ba5be0c27385a092ebd5
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-15 16:15:10 +0200

    creato comp della show

---
+++ resources/js/components/pages/DetailsComp.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="home">
+        PAGINA DETAILS
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'DetailsComp',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.home{
+    background-color: #161E2B;
+    display: flex;
+    color: aqua;
+    height: 90vh; /* DA TOGLIERE DOPO */
+    justify-content: center;
+    align-items: center;
+    font-size: 5rem;
+}
+
+</style>
